c8d0d0638ee9a93fbcc131fddbb0594e29db06cd
Rukudzo Wakandigara <52113767+Wakating@users.noreply.github.com>
2021-10-15 17:00:17 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,204 @@
+const game = () => {
+  let pScore = 0;
+  let cScore = 0;
+
+  //Start the Game
+  const startGame = () => {
+    const playBtn = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
+
+    playBtn.addEventListener("click", () => {
+      introScreen.classList.add("fadeOut");
+      match.classList.add("fadeIn");
+    });
+  };
+  //Play Match
+  const playMatch = () => {
+    const options = document.querySelectorAll(".options button");
+    const playerHand = document.querySelector(".player-hand");
+    const computerHand = document.querySelector(".computer-hand");
+    const hands = document.querySelectorAll(".hands img");
+
+    hands.forEach(hand => {
+      hand.addEventListener("animationend", function() {
+        this.style.animation = "";
+      });
+    });
+    //Computer Options
+    const computerOptions = ["rock", "paper", "scissors"];
+
+    options.forEach(option => {
+      option.addEventListener("click", function() {
+        //Computer Choice
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber];
+
+        setTimeout(() => {
+          //Here is where we call compare hands
+          compareHands(this.textContent, computerChoice);
+          //Update Images
+          playerHand.src = `./assets/${this.textContent}.png`;
+          computerHand.src = `./assets/${computerChoice}.png`;
+
+          setTimeout(() => {
+           
+            
+            playerHand.src = `./assets/rock.png`;
+            computerHand.src = `./assets/rock.png`;
+  
+          }, 2000);
+         
+        }, 2000);
+        //Animation
+        
+        
+
+      playerHand.style.animation = "shakePlayer 2s ease";
+        computerHand.style.animation = "shakeComputer 2s ease";
+
+
+      
+      });
+    });
+  };
+
+  const updateScore = () => {
+    const playerScore = document.querySelector(".player-score p");
+    const computerScore = document.querySelector(".computer-score p");
+    playerScore.textContent = pScore;
+    computerScore.textContent = cScore;
+  };
+
+  const compareHands = (playerChoice, computerChoice) => {
+    //Update Text
+    const winner = document.querySelector(".winner");
+    //Checking for a tie
+    if (playerChoice === computerChoice) {
+      winner.textContent = "It is a tie";
+      setTimeout(() => {
+        // Change hands back to Rock, Fade out Winner, Change text and Fade In
+        winner.classList.add("fadeOut");
+        winner.textContent="Choose your option";
+        winner.classList.remove("fadeOut");
+        winner.classList.add("fadeIn")
+        playerHand.classList.add("fadeOut");
+
+      }, 2000);
+
+    
+      
+
+
+
+
+      //WAKA //////////////////////
+      return;
+    }
+    //Check for Rock
+    if (playerChoice === "rock") {
+      if (computerChoice === "scissors") {
+        winner.textContent = "Player Wins";
+        pScore++;
+        updateScore();
+        setTimeout(() => {
+          // Change hands back to Rock, Fade out Winner, Change text and Fade In
+          winner.classList.add("fadeOut");
+          winner.textContent="Choose your option";
+        winner.classList.remove("fadeOut");
+          winner.classList.add("fadeIn")
+          playerHand.classList.add("fadeOut");
+        }, 2000);
+     
+  
+        return;
+      } else {
+        winner.textContent = "Computer Wins";
+        cScore++;
+        updateScore();
+        setTimeout(() => {
+          // Change hands back to Rock, Fade out Winner, Change text and Fade In
+          winner.classList.add("fadeOut");
+          winner.textContent="Choose your option";
+         winner.classList.remove("fadeOut");
+         winner.classList.add("fadeIn")
+         playerHand.classList.add("fadeOut");
+        }, 2000);
+  
+  
+        return;
+      }
+    }
+    //Check for Paper
+    if (playerChoice === "paper") {
+      if (computerChoice === "scissors") {
+        winner.textContent = "Computer Wins";
+        cScore++;
+        updateScore();
+        setTimeout(() => {
+          // Change hands back to Rock, Fade out Winner, Change text and Fade In
+          winner.classList.add("fadeOut");
+          winner.textContent="Choose your option";
+          winner.classList.remove("fadeOut");
+          winner.classList.add("fadeIn")
+          playerHand.classList.add("fadeOut");
+        }, 2000);
+ 
+        return;
+      } else {
+        winner.textContent = "Player Wins";
+        pScore++;
+        updateScore();
+        setTimeout(() => {
+          // Change hands back to Rock, Fade out Winner, Change text and Fade In
+          winner.classList.add("fadeOut");
+          winner.textContent="Choose your option";
+          winner.classList.remove("fadeOut");
+          winner.classList.add("fadeIn")
+          playerHand.classList.add("fadeOut");
+        }, 2000);
+     
+        return;
+      }
+    }
+    //Check for Scissors
+    if (playerChoice === "scissors") {
+      if (computerChoice === "rock") {
+        winner.textContent = "Computer Wins";
+        cScore++;
+        updateScore();
+        setTimeout(() => {
+          // Change hands back to Rock, Fade out Winner, Change text and Fade In
+          winner.classList.add("fadeOut");
+          winner.textContent="Choose your option";
+          winner.classList.remove("fadeOut");
+          winner.classList.add("fadeIn")
+          playerHand.classList.add("fadeOut");
+        }, 2000);
+     
+        return;
+      } else {
+        winner.textContent = "Player Wins";
+        pScore++;
+        updateScore();
+        setTimeout(() => {
+          // Change hands back to Rock, Fade out Winner, Change text and Fade In
+          winner.classList.add("fadeOut");
+          winner.textContent="Choose your option";
+          winner.classList.remove("fadeOut");
+          winner.classList.add("fadeIn")
+          playerHand.classList.add("fadeOut");
+        }, 2000);
+    
+        return;
+      }
+    }
+  };
+
+  //Is call all the inner function
+  startGame();
+  playMatch();
+};
+
+//start the game function
+game();
